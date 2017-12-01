5fc54938c5e0ce0ce10fd9f5d05a730ce04ac613
sach11 <sachin.chopra@myntra.com>
2017-12-01 15:51:58 +0530

    removed browserify, prepublish added.

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -28,7 +27,6 @@ var IDBCache = function () {
         var currentTimeStamp = Date.now();
         var expiryTime = void 0;
         expiryTime = time * 60 * 1000;
-        expiryTime += currentTimeStamp;
         setTimeout(function () {
           _idbKeyval2.default.delete(key);
         }, expiryTime);
@@ -65,98 +63,3 @@ var IDBCache = function () {
 }();
 
 exports.default = new IDBCache();
-
-},{"idb-keyval":2}],2:[function(require,module,exports){
-(function() {
-  'use strict';
-  var db;
-
-  function getDB() {
-    if (!db) {
-      db = new Promise(function(resolve, reject) {
-        var openreq = indexedDB.open('keyval-store', 1);
-
-        openreq.onerror = function() {
-          reject(openreq.error);
-        };
-
-        openreq.onupgradeneeded = function() {
-          // First time setup: create an empty object store
-          openreq.result.createObjectStore('keyval');
-        };
-
-        openreq.onsuccess = function() {
-          resolve(openreq.result);
-        };
-      });
-    }
-    return db;
-  }
-
-  function withStore(type, callback) {
-    return getDB().then(function(db) {
-      return new Promise(function(resolve, reject) {
-        var transaction = db.transaction('keyval', type);
-        transaction.oncomplete = function() {
-          resolve();
-        };
-        transaction.onerror = function() {
-          reject(transaction.error);
-        };
-        callback(transaction.objectStore('keyval'));
-      });
-    });
-  }
-
-  var idbKeyval = {
-    get: function(key) {
-      var req;
-      return withStore('readonly', function(store) {
-        req = store.get(key);
-      }).then(function() {
-        return req.result;
-      });
-    },
-    set: function(key, value) {
-      return withStore('readwrite', function(store) {
-        store.put(value, key);
-      });
-    },
-    delete: function(key) {
-      return withStore('readwrite', function(store) {
-        store.delete(key);
-      });
-    },
-    clear: function() {
-      return withStore('readwrite', function(store) {
-        store.clear();
-      });
-    },
-    keys: function() {
-      var keys = [];
-      return withStore('readonly', function(store) {
-        // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
-        // And openKeyCursor isn't supported by Safari.
-        (store.openKeyCursor || store.openCursor).call(store).onsuccess = function() {
-          if (!this.result) return;
-          keys.push(this.result.key);
-          this.result.continue();
-        };
-      }).then(function() {
-        return keys;
-      });
-    }
-  };
-
-  if (typeof module != 'undefined' && module.exports) {
-    module.exports = idbKeyval;
-  } else if (typeof define === 'function' && define.amd) {
-    define('idbKeyval', [], function() {
-      return idbKeyval;
-    });
-  } else {
-    self.idbKeyval = idbKeyval;
-  }
-}());
-
-},{}]},{},[1]);
